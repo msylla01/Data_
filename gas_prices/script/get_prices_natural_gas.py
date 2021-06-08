b3573229eb1b461251040a7807045dc679fa4f8c
sawibrah <sawibrah@gmail.com>
2021-06-08 17:55:09 +0000

    tabular data

--- gas_prices/script/get_prices_natural_gas.py
+++ gas_prices/script/get_prices_natural_gas.py
@@ -1,9 +1,9 @@
 """
 All the data Daily Prices 'Henry Hub Natural Gas Spot Price (Dollars per Million Btu)'
 
-The script have two function and contains the function that calls the EIA API and 
-put the data in to the CSV file located in Data_wrangling_exercise/data/daily_gas_prices.csv \
-with this data.
+The script have two function  that calls the EIA API and 
+put the data in to the CSV file located in Data_wrangling_exercise/gas_prices/data/daily_gas_prices.csv 
+
 """
 
 
@@ -12,7 +12,7 @@ import json
 import pandas as pd
 
 
-def get_list_of_data(duration_type = "annually"):
+def get_list_of_data(duration_type = "daily"):
     
     """
      API call the EIA and returns a list of daily prices.
@@ -35,7 +35,7 @@ def get_list_of_data(duration_type = "annually"):
     )
     
     # Series ID for the different duration type we can get this on json request
-    series_id = {
+    series_uni_id = {
         "daily": "NG.RNGWHHD.D",
         "weekly": "NG.RNGWHHD.W",
         "monthly": "NG.RNGWHHD.M",
@@ -44,7 +44,7 @@ def get_list_of_data(duration_type = "annually"):
     
     
     # Generate the  URL  from the B_URL and series_id
-    URL  = B_URL + series_id.get(duration_type,"daily")
+    URL  = B_URL + series_uni_id.get(duration_type,"daily")
     
     #Using the python requests library and API reponse 
     #the response of request is  stored in the variable df and the format is json data as disctionary in python.
@@ -80,7 +80,7 @@ def csv_file( duration_type="daily",filepath=None):
     
     # Default File path 
     
-    path = 'data/{}_gas_prices.csv'.format(duration_type)
+    path = 'gas_prices/data/{}_gas_prices.csv'.format(duration_type)
     
     if type(filepath) == str:
         path = filepath
